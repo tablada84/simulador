c4ae59bace72a729e449f65c02233f120edaaf6c
Matias Agustin Mariño <matiasagustinmarino@MacBook-Pro-de-Matias.local>
2025-12-18 12:15:14 -0300

    arreglos

--- main.js
+++ main.js
@@ -41,8 +41,12 @@ const getProducts= async()=>{
 
     const response = await fetch("data.json");
     const data = await response.json()
-   data.forEach((product)=>{
+
+    data.forEach((product)=>{
+
+
     let content = document.createElement("div")
+
     content.className = "card"
     content.innerHTML = `
     <img src = "${product.img}"
@@ -57,7 +61,7 @@ Comprar.className = "comprar"
 
 content.append(Comprar)
 
-Comprar.addEventListener("click", ()=>{
+
   Comprar.addEventListener("click", () => {
 
     const repetir = carrito.some((repetirProduct) => repetirProduct.id === product.id);
@@ -77,47 +81,10 @@ Comprar.addEventListener("click", ()=>{
     saveLocal()
 })
 })
-})
-
 }
-getProducts()
-data.forEach((product)=>{
-    let content = document.createElement("div")
-    content.className = "card"
-    content.innerHTML = `
-    <img src = "${product.img}"
-    <h3>${product.nombre}</h3>
-    <p class="price">$${product.precio}</p>`
-
-    shopContent.append(content)
-
-let Comprar = document.createElement("button")
-Comprar.innerText = "comprar"
-Comprar.className = "comprar"
-
-content.append(Comprar)
-
-Comprar.addEventListener("click", ()=>{
-  Comprar.addEventListener("click", () => {
-
-    const repetir = carrito.some((repetirProduct) => repetirProduct.id === product.id);
-    repetir ? carrito.map((prod) => prod.id === product.id && prod.cantidad++) : carrito.push({
-      id: product.id,
-      img: product.img,
-      nombre: product.nombre,
-      precio: product.precio,
-      cantidad: product.cantidad,
-      title: product.title,
-    });
 
 
-    console.log(carrito)
-    console.log(carrito.length)
-    carritoCounter();
-    saveLocal()
-})
-})
-})
+
 
 const saveLocal=()=>{ 
 
@@ -125,3 +92,5 @@ localStorage.setItem("carrito", JSON.stringify(carrito))
 }
 
 JSON.parse(localStorage.getItem("carrito"))
+
+getProducts()
